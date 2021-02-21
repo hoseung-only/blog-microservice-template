e6e1dbbb3a9e26da15c2760092370e5a197fcd7b
HoseungJang <hsjang.dev@gmail.com>
2021-02-21 16:59:56 +0900

    apply test routers

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
 
 import { applyHelloworldRouters } from "./routers/helloworld";
+import { applyTestRouters } from "./routers/test";
 import { applyErrorHandlers } from "./errorHandlers";
 
 export const getRootRouter = () => {
   const router = Router();
 
   applyHelloworldRouters(router);
+  applyTestRouters(router);
   applyErrorHandlers(router);
 
   return router;
