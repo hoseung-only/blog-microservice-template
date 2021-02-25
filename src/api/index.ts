615b3795ba0fc367bfcfe930fba692b1dedf30c1
HoseungJang <hsjang.dev@gmail.com>
2021-02-25 22:04:02 +0900

    remove test router

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,14 +1,12 @@
 import { Router } from "express";
 
 import { applyHelloworldRouters } from "./routers/helloworld";
-import { applyTestRouters } from "./routers/test";
 import { applyErrorHandlers } from "./errorHandlers";
 
 export const getRootRouter = () => {
   const router = Router();
 
   applyHelloworldRouters(router);
-  applyTestRouters(router);
   applyErrorHandlers(router);
 
   return router;
